9ad992a9251c5e8de6241cb5304fa28ce94607c1
lewinskie254 <142066223+lewinskie254@users.noreply.github.com>
2024-01-25 21:33:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let items = document.querySelectorAll('.list .item')
+let next = document.getElementById('next')
+let prev = document.getElementById('prev')
+let thumbnails = document.querySelectorAll('.thumbnail .item-mini')
+
+
+// config param 
+let countItem = items.length
+let itemActive = 0; 
+
+//event next click 
+next.onclick = () => {
+    itemActive = itemActive + 1
+    if (itemActive >= countItem){
+        itemActive = 0 
+    }
+    showSlider()
+}
+
+prev.onclick = () => {
+    itemActive = itemActive - 1
+    if (itemActive < 0){
+        itemActive = countItem - 1
+    }
+    showSlider()
+}
+
+const showSlider = () => {
+    let itemActiveOld = document.querySelector('.slider .list .item.active')
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item-mini.active')
+    itemActiveOld.classList.remove('active')
+    thumbnailActiveOld.classList.remove('active')
+
+
+    //active new item 
+    items[itemActive].classList.add('active')
+    thumbnails[itemActive].classList.add('active')
+}
+
+//click thumbnails
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () =>{
+        itemActive = index
+        showSlider()
+    })
+})
